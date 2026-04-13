d963aefe7e462436ed8c0e0d8d64d066ebb360c7
Jenaro Buffa <jenitobuffa@gmail.com>
2026-04-13 20:34:34 -0300

    1er ejercicio javascript hecho

---
+++ ejercicios/c03-javascript/app.js
@@ -0,0 +1,14 @@
+const nombre = "Jenaro";   // datos:
+let edad = 20;
+const materia = "Desarrollo de software";
+
+//contador + 3:
+contador = 0
+contador + 1; 
+contador +1 ; 
+contador +1;
+
+
+console.log(`Me llamo ${nombre} , tengo ${edad} anios y estudio la materia ${materia}`);
+console.log("el resultado final del contador es : ", contador )
+
